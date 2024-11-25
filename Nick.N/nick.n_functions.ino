1f02abf2dd3f8a8d0ac00e3e32c4f6dcdad782d8
Nick N <159206669+NickWeiHaoNi@users.noreply.github.com>
2024-11-25 13:59:46 +1100

    got rid of button

--- Nick.N/nick.n_functions.ino
+++ Nick.N/nick.n_functions.ino
@@ -2,11 +2,9 @@ void attachsetup() {
  servo1.attach(5);
   servo2.attach(4);
   servo3.attach(3);
-pinMode(buttonPin, INPUT);
 }
 
 void servorotations (){
- if (buttonState == HIGH) { // Button is pressed
     servo1.write(90);  // move servo1 to 90 degrees
     servo2.write(90);  // move servo2 to 90 degrees
     servo3.write(90);  // move servo3 to 90 degrees
