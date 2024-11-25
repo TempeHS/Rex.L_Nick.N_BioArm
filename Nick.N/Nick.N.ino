df232e692d4e55ead7949d79d9212c44e5a731c9
Nick N <159206669+NickWeiHaoNi@users.noreply.github.com>
2024-11-25 13:57:46 +1100

    moved to functions

--- Nick.N/Nick.N.ino
+++ Nick.N/Nick.N.ino
@@ -10,27 +10,14 @@ int buttonState = 0;     // variable to store the button state
 
 void setup() {
   // Attach servos to their respective pins
-  servo1.attach(5);
-  servo2.attach(4);
-  servo3.attach(3);
-
-  // Set button pin as input
-  pinMode(buttonPin, INPUT);
+  attachsetup();
 }
 
 void loop() {
   // Read the state of the button
   buttonState = digitalRead(buttonPin);
 
-  if (buttonState == HIGH) { // Button is pressed
-    servo1.write(90);  // move servo1 to 90 degrees
-    servo2.write(90);  // move servo2 to 90 degrees
-    servo3.write(90);  // move servo3 to 90 degrees
-  } else { // Button is not pressed
-    servo1.write(0);   // move servo1 back to 0 degrees
-    servo2.write(0);   // move servo2 back to 0 degrees
-    servo3.write(0);   // move servo3 back to 0 degrees
-  }
+  servorotations();
 
   // Small delay to debounce the button
   delay(10);
