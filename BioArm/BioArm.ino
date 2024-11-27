94f4912cf4930d61034d49c18d60ab515bcd3bb0
Nick N <159206669+NickWeiHaoNi@users.noreply.github.com>
2024-11-27 14:44:29 +1100

    idk what i did

--- BioArm/BioArm.ino
+++ BioArm/BioArm.ino
@@ -1,20 +1,3 @@
-/*Rex:
-Doc: https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/tree/main/TempeHS_Sensor_Catalogue/Sensor_Kit/Ultrasonic_Ranger
-Ultra pin 6
-Digit Display pin 2
-
-Nick:
-Doc: https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/tree/main/TempeHS_Sensor_Catalogue/Sensor_Kit/Servo_Motor
-servo 1 pin 3
-servo 2 pin 4
-servo 3 pin 5
-servo 4 pin 6
-servo 5 pin 7
-1x Ultrasonic Sensor
-5x Servo Motors
-1x 4-Digit Display
-*/
-
 #include "Ultrasonic.h"
 #include <Servo.h>
 
