885aa7dea954c416c66b4dd379fbc4f69e6989c1
Rex Liang <159222539+Rex0Liang@users.noreply.github.com>
2024-12-05 10:33:45 +1100

    Started integrating OLED display

--- Rex.L/Rex.L.ino
+++ Rex.L/Rex.L.ino
@@ -1,11 +1,30 @@
 #include "Ultrasonic.h"
+#include <Wire.h>
+#include <U8g2lib.h>
+#include <Arduino.h>
 
 Ultrasonic distanceSensor(6);
 
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);  // High speed I2C
+
 void setup() {
-  sensorsetup();
+  //sensorsetup();
+  u8g2.begin();
 }
 
 void loop() {
-  sensorRead();
+  //sensorRead();
+  u8g2.clearBuffer();                  // clear the internal memory
+  u8g2.setFont(u8g2_font_ncenB08_tr);  // choose a suitable font
+  u8g2.setCursor(0, 10);              // Set cursor to first line
+  u8g2.print("Hello World!");         // write something to the internal memory
+  u8g2.sendBuffer();                  // transfer internal memory to the display
+  delay(1000);  
 }
+
+
+    
+
+
+
+
